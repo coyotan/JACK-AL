eca14d1f004a9cbbe6774cc0952aadc0471e7322
Wesley O'Brien <thefirstfoxdev@gmail.com>
2020-10-10 18:15:17 -0400

    Added !add5eCal command to DND Module

--- bot/responders/dndResponder.go
+++ bot/responders/dndResponder.go
@@ -1,15 +1,31 @@
 package responders
 
 import (
+	"errors"
 	"github.com/CoyoTan/JACK-AL/bot/responders/dnd5e"
 	"github.com/bwmarrin/discordgo"
+	"github.com/txgruppi/parseargs-go"
 )
 
 func init() {
 	addInitModListener("DND5e", dnd5e.InitDnd)
+	addCreateListener("add5eCalendar", addDndGuildCalender)
+	addCreateListener("add5ecalendar", addDndGuildCalender)
+	addCreateListener("add5eCal", addDndGuildCalender)
+	addCreateListener("add5ecal", addDndGuildCalender)
 }
 
 func addDndGuildCalender(m *discordgo.Message) (err error) {
+
+	args, err := parseargs.Parse(m.Content)
+
+	if err != nil {
+		return
+	} else if len(args[1]) < 10 {
+		return errors.New("invalid Google Calendar id")
+	}
+
+	dnd5e.AddCalBuBuild(m.GuildID, args[1])
 
 	return
 }
