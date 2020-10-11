e9e4ec853677d2e60bfc5f828168a784e685a123
Wesley O'Brien <thefirstfoxdev@gmail.com>
2020-10-10 20:25:29 -0400

    Completed P1 GetDailyEvents function

--- bot/responders/dnd5e/gcore/DailySummary.go
+++ bot/responders/dnd5e/gcore/DailySummary.go
@@ -2,21 +2,27 @@ package gcore
 
 import (
 	"encoding/json"
+	"strings"
 	"time"
 )
 
 var Cal = e5Core.dCore.GetGCore()
 
+//GetDailyEventSummary is called by a 24 hour recurring function. It MUST run before 0600, but specific timing is otherwise unimportant.
 func GetDailyEventSummary(calenderID string) {
 
 	est, _ := time.LoadLocation("America/New_York")
 	t0 := time.Now().In(est)
-	t1 := t0.Add(time.Hour * 24).Format(time.RFC3339)
-
 	y, m, d := t0.Date()
 	t0d := time.Date(y, m, d, 0, 0, 0, 0, est)
+	t1 := t0d.Add(time.Hour * 24).Format(time.RFC3339)
 
 	//Verify the guild is legit
+	gCal, err := verifyGuildCalender(calenderID)
+
+	if err != nil {
+		jackal.Logger.Error.Printf("There was a critical error verifying the Guild Calendar\n %v", err)
+	}
 
 	evts, err := Cal.Events.List(calenderID).ShowDeleted(false).TimeMax(t1).TimeMin(t0d.Format(time.RFC3339)).SingleEvents(true).OrderBy("startTime").Do()
 
@@ -24,29 +30,50 @@ func GetDailyEventSummary(calenderID string) {
 		jackal.Logger.Error.Println("There was a error reading events from the calendar: ", err)
 	}
 
-	for k, v := range evts.Items {
-		if len(v.Description) < 10 {
-			//Condition for "This is probably not a valid json"
-		} else {
-
-		}
+	dailyEvts := &DailyCalendarEvent{
+		GuildCal: gCal,
 	}
 
+	for _, v := range evts.Items {
+		if len(v.Description) < 10 {
+			//Condition for "This is probably not a valid json"
+		} else if strings.ToLower(string(v.Summary[0:11])) == "daily event" {
+			err = json.Unmarshal([]byte(v.Description), dailyEvts)
+
+			if err != nil {
+				jackal.Logger.Error.Printf("There was a criticl error when attempting to unmarshal today's event for %s.\n%v", calenderID, err)
+			}
+
+		} else {
+			newSpecEvt := SpecialEvent{}
+
+			err = json.Unmarshal([]byte(v.Description), &newSpecEvt)
+
+			if err != nil {
+				jackal.Logger.Error.Printf("There was an error attempting to unmarshal an event in the %s Calendar.\n %v", calenderID, err)
+			}
+
+			newSpecEvt.GuildCal = gCal
+			e5Core.GuildCalendars[calenderID].SpecEvts = append(e5Core.GuildCalendars[calenderID].SpecEvts, newSpecEvt)
+
+			//We will need to create goroutines for each of these later, so we can perform the appropriate actions
+		}
+	}
 }
 
-func verifyGuildCalender(calenderID string) (err error) {
+func verifyGuildCalender(calenderID string) (newCal *GuildCalender, err error) {
 	guildCal, err := Cal.Calendars.Get(calenderID).Do()
 
 	if err != nil {
 		jackal.Logger.Error.Println("There was a critical error verifying the DND Calender JSON for ", calenderID)
 	}
 
-	newCal := &GuildCalender{}
+	newCal = &GuildCalender{}
 
 	err = json.Unmarshal([]byte(guildCal.Description), &newCal)
 
 	if err != nil {
-		jackal.Logger.Error.Println("There was a critical error unmarshalling the guildCalender Description", err)
+		jackal.Logger.Error.Println("There was a critical error when trying to unmarshal the guildCalender Description", err)
 		return
 	} else {
 		e5Core.GuildCalendars[calenderID] = newCal
--- bot/responders/dnd5e/gcore/interfaces.go
+++ bot/responders/dnd5e/gcore/interfaces.go
@@ -6,7 +6,6 @@ type dndCore interface {
 	SetGCore(core *calendar.Service)
 	GetGCore() *calendar.Service
 	GetDndDir() string
-	GetGuildMap() map[string]string
 	SetDndGuildCal(DndGuildCalendersJSON string)
 	GetDndGuildCal() string
 }
