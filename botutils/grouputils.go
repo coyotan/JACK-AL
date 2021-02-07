4c34b108a8fa97b169f6823ab54a03b9dd2958d4
Wesley O'Brien <thefirstfoxdev@gmail.com>
2021-02-06 20:38:04 -0500

    Trying to make this run again.

--- botutils/grouputils.go
+++ botutils/grouputils.go
@@ -4,7 +4,7 @@ import "github.com/bwmarrin/discordgo"
 
 var (
 	//Byte codes for discord permissions.
-	administrator = 0x8
+	administrator int64 = 0x8
 )
 
 //CheckAdminPermissions will DEFINITELY need to be updated in the future. I think we might need to use Presences, which is a privileged intent. This means eventually, we will need to pass session into here.
