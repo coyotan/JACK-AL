1c40a9a3d1df6b41e27b4dea2b695daa6e997200
Wesley O'Brien <thefirstfoxdev@gmail.com>
2021-02-11 18:37:51 -0500

    Added GenEmbed for IncomePayment

--- bot/responders/dnd5e/structs.go
+++ bot/responders/dnd5e/structs.go
@@ -23,14 +23,20 @@ func (b *BasicNotif) Validate() (valid bool, reason error) {
 
 ///TODO: Test this function.
 func (b *BasicNotif) GenerateEmbed() (embed *discordgo.MessageEmbed, err error) {
-	embed.Title = b.Title
-	embed.Description = b.Description
-	embed.Color, err = toIntColor(hexaNumberToInteger(b.Color))
-	embed.Image = &discordgo.MessageEmbedImage{URL: b.Picture}
-	embed.Footer = &discordgo.MessageEmbedFooter{
-		Text: "JACK-AL Framework | DND5e Extension.",
+
+	if valid, reason := b.Validate(); valid {
+		embed.Title = b.Title
+		embed.Description = b.Description
+		embed.Color, err = toIntColor(hexaNumberToInteger(b.Color))
+		embed.Image = &discordgo.MessageEmbedImage{URL: b.Picture}
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text:    "JACK-AL Framework | DND5e Extension.",
+			IconURL: "https://cloud.coyotech.dev/index.php/s/7jsto99sqHzpi9P/preview",
+		}
+		return embed, nil
+	} else {
+		return nil, reason
 	}
-	return
 }
 
 //Add custom/predefined Header and Footer.
@@ -61,16 +67,52 @@ func (q *QuestBoard) Validate() (valid bool, reason error) {
 	return true, nil
 }
 
+///TODO: Make this one a complicated one. This one should have way more to it.
+func (q *QuestBoard) GenerateEmbed() (embed *discordgo.MessageEmbed, err error) {
+	if valid, reason := q.Validate(); valid {
+		embed.Title = q.Title
+		embed.Description = q.Description
+		embed.Color, err = toIntColor(hexaNumberToInteger(q.Color))
+		embed.Image = &discordgo.MessageEmbedImage{URL: q.Picture}
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text:    "JACK-AL Framework | DND5e Extension.",
+			IconURL: "https://cloud.coyotech.dev/index.php/s/7jsto99sqHzpi9P/preview",
+		}
+		return embed, nil
+	} else {
+		return nil, reason
+	}
+}
+
 //Add custom/predefined Header and Footer.
 type Daily struct {
 	Role []string `json:"Role, omitempty"`
 	BasicNotif
 }
 
+///TODO: Make this one ping people.
+func (d *Daily) GenerateEmbed() (embed *discordgo.MessageEmbed, err error) {
+
+	if valid, reason := d.Validate(); valid {
+		embed.Title = d.Title
+		embed.Description = d.Description
+		embed.Color, err = toIntColor(hexaNumberToInteger(d.Color))
+		embed.Image = &discordgo.MessageEmbedImage{URL: d.Picture}
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text:    "JACK-AL Framework | DND5e Extension.",
+			IconURL: "https://cloud.coyotech.dev/index.php/s/7jsto99sqHzpi9P/preview",
+		}
+		return embed, nil
+	} else {
+		return nil, reason
+	}
+}
+
 type Taxes struct {
 	Announced   bool   `json:"Announced"`
 	Title       string `json:"Title, omitempty"`
 	Description string `json:"Description, omitempty"`
+	Color       string `json:"Color, omitempty"`
 	Percentage  int    `json:"Percentage, omitempty"`
 	Amount      int    `json:"Amount, omitempty"`
 }
@@ -87,10 +129,36 @@ func (t *Taxes) Validate() (valid bool, reason error) {
 	return true, nil
 }
 
+///TODO: Test this function.
+func (t *Taxes) GenerateEmbed() (embed *discordgo.MessageEmbed, err error) {
+
+	if valid, reason := t.Validate(); valid {
+		embed.Title = t.Title
+		if len(t.Description) > 1 {
+			embed.Description = t.Description
+		}
+		if len(t.Color) > 1 {
+			embed.Color, err = toIntColor(hexaNumberToInteger(t.Color))
+		} else {
+			embed.Color = 0x00c5ff
+		}
+
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text:    "JACK-AL Framework | DND5e Extension.",
+			IconURL: "https://cloud.coyotech.dev/index.php/s/7jsto99sqHzpi9P/preview",
+		}
+
+		return embed, nil
+	} else {
+		return nil, reason
+	}
+}
+
 type IncomePayment struct {
 	Announced   bool   `json:"Announced"`
 	Title       string `json:"Title, omitempty"`
 	Description string `json:"Description, omitempty"`
+	Color       string `json:"Color, omitempty"`
 	IncomeTax   int    `json:"IncomeTax, omitempty"`
 }
 
@@ -102,4 +170,26 @@ func (i *IncomePayment) Validate() (valid bool, reason error) {
 		}
 	}
 	return true, nil
+}
+
+func (i *IncomePayment) GenerateEmbed() (embed *discordgo.MessageEmbed, err error) {
+	if valid, reason := i.Validate(); valid {
+		embed.Title = i.Title
+		if len(i.Description) > 1 {
+			embed.Description = i.Description
+		}
+		if len(i.Color) > 1 {
+			embed.Color, err = toIntColor(hexaNumberToInteger(i.Color))
+		} else {
+			embed.Color = 0x00c5ff
+		}
+
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text:    "JACK-AL Framework | DND5e Extension.",
+			IconURL: "https://cloud.coyotech.dev/index.php/s/7jsto99sqHzpi9P/preview",
+		}
+		return embed, nil
+	} else {
+		return nil, reason
+	}
 }
