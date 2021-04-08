c0ab5e7131855b41ce1080272c7d695de70b0372
Wesley O'Brien <thefirstfoxdev@gmail.com>
2021-04-07 23:20:17 -0400

    Fixing some stupid shit.

--- bot/handlers.go
+++ bot/handlers.go
@@ -72,21 +72,21 @@ func deleteDispatch(_ *discordgo.Session, deleted *discordgo.MessageDelete) {
 
 //Thanks to the new "BeforeUpdate" method, this is all this function really needs to be! It's so much simpler now!
 func editDispatch(_ *discordgo.Session, updated *discordgo.MessageUpdate) {
-	if updated.Author.ID != jackal.Discord.User.ID {
-		var totalListeners = 0
-		//If we cannot find the specific command we are looking for, tell EVERYONE what we found...
-		for _, v := range jackal.Discord.EditListeners {
-			err := v(updated)
+	//Commented this line out for now since the bot doesn't edit its own messages, and it is causing me anger. if updated.Message.Author.ID != jackal.Discord.User.ID {
+	var totalListeners = 0
+	//If we cannot find the specific command we are looking for, tell EVERYONE what we found...
+	for _, v := range jackal.Discord.EditListeners {
+		err := v(updated)
 
-			if err != nil {
-				jackal.Logger.Error.Println("Edit responder is 10-33", err)
-			} else {
-				totalListeners++
-			}
+		if err != nil {
+			jackal.Logger.Error.Println("Edit responder is 10-33", err)
+		} else {
+			totalListeners++
 		}
-
-		jackal.Logger.Console.Println("Dispatched to ", totalListeners, " listeners. All responders are 10-8.")
 	}
+
+	jackal.Logger.Console.Println("Dispatched to ", totalListeners, " listeners. All responders are 10-8.")
+	//}
 }
 
 func addReactionDispatch(_ *discordgo.Session, reaction *discordgo.MessageReactionAdd) {
--- bot/responders/leaveResponder.go
+++ bot/responders/leaveResponder.go
@@ -20,21 +20,22 @@ func init() {
 }
 
 func responderLeave(message *discordgo.Message) (err error) {
-	jackal.Logger.Console.Println("Discord message received: ", message.Content)
-	if isAdm, err := botutils.CheckAdminPermissions(jackal.Discord.Session, message); isAdm {
-		if strings.ToLower(message.Content[len(jackal.Discord.CommandPrefix):]) == "leave" {
-			jackal.Discord.Session.ChannelMessageSend(message.ChannelID, "Leaving now!")
-			jackal.Discord.Session.Close()
-			jackal.Logger.Info.Println("Peacefully closing JACK-AL. Exit Code 100")
-			jackal.DB.Close()
-			os.Exit(100)
+	if strings.ToLower(message.Content[len(jackal.Discord.CommandPrefix):]) == "leave" {
+		jackal.Logger.Console.Println("Discord message received: ", message.Content)
+		if isAdm, err := botutils.CheckAdminPermissions(jackal.Discord.Session, message); isAdm {
+			if strings.ToLower(message.Content[len(jackal.Discord.CommandPrefix):]) == "leave" {
+				jackal.Discord.Session.ChannelMessageSend(message.ChannelID, "Leaving now!")
+				jackal.Discord.Session.Close()
+				jackal.Logger.Info.Println("Peacefully closing JACK-AL. Exit Code 100")
+				jackal.DB.Close()
+				os.Exit(100)
+			}
+		} else if err != nil {
+			jackal.Logger.Error.Println("There was an error validating the permissions of user ", message.Author.Username+"#"+message.Author.Discriminator, " when they attempted to run the leave command.")
+		} else {
+			jackal.Discord.Session.ChannelMessageSend(message.ChannelID, "You do not have permission to use that command")
+			jackal.Logger.Warn.Println(message.Author.Username+"#"+message.Author.Discriminator, " attempted to run privileged command 'leave' but was rejected due to insufficient permissions.")
 		}
-	} else if err != nil {
-		jackal.Logger.Error.Println("There was an error validating the permissions of user ", message.Author.Username+"#"+message.Author.Discriminator, " when they attempted to run the leave command.")
-	} else {
-		jackal.Discord.Session.ChannelMessageSend(message.ChannelID, "You do not have permission to use that command.")
-		jackal.Logger.Warn.Println(message.Author.Username+"#"+message.Author.Discriminator, " attempted to run privileged command 'leave' but was rejected due to insufficient permissions.")
 	}
-
 	return
 }
