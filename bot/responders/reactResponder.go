b8540f7f44f52a38edd5829e3e21a21bb388387f
Wesley O'Brien <thefirstfoxdev@gmail.com>
2021-04-07 23:21:24 -0400

    Added React Responder and most of the foundation to proceed. Also fixed stupid bugs pointed out by annoying users. Thank you tiny children, return to your parents.

--- bot/responders/reactResponder.go
+++ bot/responders/reactResponder.go
@@ -1,8 +1,11 @@
 package responders
 
 import (
+	"errors"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/txgruppi/parseargs-go"
+	"strings"
 )
 
 //DOCUMENTATION:
@@ -21,8 +24,82 @@ func init() {
 }
 
 func responderReactionAction(message *discordgo.Message) (err error) {
-	_, err = jackal.Discord.Session.ChannelMessageSend(message.ChannelID, "This function has not yet been implemented.")
-	fmt.Println(message.Content)
+
+	var watchMessageID string
+	var actionWord = true
+	var reaction = false
+
+	var action string
+	var argument string
+
+	if len(message.Content) > 12 && strings.ToLower(message.Content[len(jackal.Discord.CommandPrefix):12]) == "reactionact" {
+		//Parseargs in order: reactionact messageID Action1 Reaction Action2 Reaction Action3 Reaction [...]
+
+		if len(message.Content) > 31 {
+			//Get the messageID
+			watchMessageID = message.Content[13:31]
+
+			//TODO: Add reaction to the message listed here.
+
+			//Process every third item from the
+			args, err := parseargs.Parse(message.Content[31:])
+
+			if err != nil {
+				return err
+			} else if len(args) < 3 {
+				//Tell the user they fucked up here in a Discord message.
+				return errors.New("not enough information provided to create a reaction action")
+			}
+
+			for index, value := range args {
+
+				if index == 0 {
+					actionWord = true
+					reaction = false
+				}
+
+				if actionWord && !reaction {
+					fmt.Println("Action is " + value)
+					action = value
+					actionWord = !actionWord
+				} else if !actionWord && !reaction {
+					fmt.Println("Argument is " + value)
+					argument = value
+					reaction = !reaction
+				} else if !actionWord && reaction {
+					fmt.Println("Reaction is " + value)
+
+					//TODO: Commit action using action, argument, and value as function arguments.!
+					err = doWork(action, argument, value)
+				}
+			}
+
+			fmt.Println(watchMessageID)
+
+		} else { //If the user did not post something that passes the minimal length of a action and a reaction.
+
+			return errors.New("not enough information provided to create a reaction action")
+		}
+		_, err = jackal.Discord.Session.ChannelMessageSend(message.ChannelID, "We need to add more stuff for this to work!")
+		fmt.Println(message.Content)
+	}
+
+	return
+}
+
+func doWork(action string, argument string, reaction string) (err error) {
+	switch strings.ToLower(action) {
+	case "addrole":
+		//Actually add their role.
+	case "vote":
+		//To be implemented at a very later date.
+	case "setprefix":
+		//Literally set the prefix for the username.
+	case "setsuffix":
+		//Literally set the suffix for the username
+	case "void":
+		//Do nothing.
+	}
 	return
 }
 
