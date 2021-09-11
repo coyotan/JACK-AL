d25f067676e672d7430cb303337f47b7efd959ed
Wesley O'Brien <thefirstfoxdev@gmail.com>
2021-09-10 23:01:09 -0400

    I realized I didn't push this before I started running security tests....

--- bot/responders/dnd5e/utils.go
+++ bot/responders/dnd5e/utils.go
@@ -1,15 +1,10 @@
 package dnd5e
 
 import (
-	"errors"
 	"strconv"
 )
 
 func toIntColor(num string) (numColor int, err error) {
-	output, err := strconv.ParseInt(num, 0, 64)
-	if output < 1 {
-		return int(output), err
-	} else {
-		return 0, errors.New("color cannot be less than 0")
-	}
+	output, err := strconv.ParseUint(num, 0, 64)
+	return int(output), err
 }
